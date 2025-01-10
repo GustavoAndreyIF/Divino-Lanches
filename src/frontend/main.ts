bf512b59ef24255a1eeb577ef4e0de91a8b59a40
Gustavo Andrey <gustavoandrey@escolar.ifrn.edu.br>
2025-01-10 19:39:32 -0300

    mais do front

--- src/frontend/main.ts
+++ src/frontend/main.ts
@@ -1,15 +1,19 @@
 import { ApiService } from "./services/apiService.js";
 import { ProdutoService } from "./services/produtoService.js";
+import { ProdutoController } from "./controllers/produtoController.js";
 import { DomProduto } from "./utils/domProduto.js";
 
 (async () => {
-  const apiService = new ApiService("http://localhost:3000");
-  const produtoService = new ProdutoService(apiService);
+	const apiService = new ApiService("http://localhost:3000");
+	const produtoService = new ProdutoService(apiService);
+	const produtoController = new ProdutoController(produtoService);
 
-  try {
-    const produtos = await produtoService.getTodosProdutos();
-    DomProduto.renderProdutos(produtos);
-  } catch (error) {
-    console.error("Erro ao consumir a API:", error);
-  }
+	try {
+		const produtos = await produtoService.getTodosProdutos();
+		DomProduto.renderProdutos(produtos);
+	} catch (error) {
+		console.error("Erro ao consumir a API:", error);
+	}
+
+	(window as any).produtoController = produtoController;
 })();
