eea801e84063c5b6f4954b6566d80c89eed8a340
TrojanN63 <alves.tavares@escolar.ifrn.edu.br>
2025-01-10 21:03:33 -0300

    modelos de pedido e cliente

---
+++ src/backend/models/clientes_model.ts
@@ -0,0 +1,17 @@
+import { queryCallback } from "mysql";
+import db from "../config/db.js";
+
+class ClienteModel{
+    constructor(){}
+    get_Cliente(id_cliente: number, callback:queryCallback) {
+        const query = `SELECT * FROM tb_clientes WHERE id_cliente = ${id_cliente}`;
+        db.query(query, callback);
+    };
+    create_Cliente(cliente_email: string, cliente_log_senha: string, nm_cliente: string, callback:queryCallback){
+        const query = `INSERT INTO tb_clientes (cliente_email, cliente_log_senha, nm_cliente) VALUES (${cliente_email}, ${cliente_log_senha}, ${nm_cliente}`;
+        db.query(query, callback)
+    };
+    delete_cliente(id_cliente: number, callback:queryCallback){
+        const query = `DELETE from tb_clientes WHERE id_cliente = ${id_cliente}`
+    }
+}
---
+++ src/backend/models/pedidos_model.ts
@@ -0,0 +1,22 @@
+import { queryCallback } from "mysql";
+import db from "../config/db.js";
+class PedidoModel{
+    constructor(){}
+    get_Pedido(id_pedido: number, callback:queryCallback) {
+        const query = `SELECT * FROM tb_pedidos WHERE id_pedido = ${id_pedido}`;
+        db.query(query, callback);
+    };
+    create_Pedido(id_cliente: number, id: number, id_produto: number, qt_pedido: number, status_pedido: string, callback:queryCallback){
+        const query = `INSERT INTO tb_pedidos (id, id_cliente, id_Produto, Qt_Pedido, status_pedido) VALUES (${id}, ${id_cliente}, ${id_produto}, ${qt_pedido}, ${status_pedido})`;
+        db.query(query, callback)
+    };
+    delete_Pedido(id_pedido: number, callback:queryCallback) {
+        const query = `DELETE FROM tb_pedidos WHERE id_pedido = ${id_pedido}`;
+        db.query(query, callback);
+    }
+    alt_status_pedido(id_pedido: number, status_pedido: string, callback:queryCallback){
+        const query = `UPDATE tb_pedidos WHERE id_pedido = ${id_pedido} SET status_pedido = ${status_pedido}`
+        db.query(query, callback)
+    }
+}
+export default PedidoModel;
