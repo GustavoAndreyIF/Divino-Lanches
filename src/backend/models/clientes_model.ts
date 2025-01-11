5ff96ed32f33fbb72752c7862399718ea270b8d1
TrojanN63 <alves.tavares@escolar.ifrn.edu.br>
2025-01-10 21:16:11 -0300

    mensagem

--- src/backend/models/clientes_model.ts
+++ src/backend/models/clientes_model.ts
@@ -9,9 +9,10 @@ class ClienteModel{
     };
     create_Cliente(cliente_email: string, cliente_log_senha: string, nm_cliente: string, callback:queryCallback){
         const query = `INSERT INTO tb_clientes (cliente_email, cliente_log_senha, nm_cliente) VALUES (${cliente_email}, ${cliente_log_senha}, ${nm_cliente}`;
-        db.query(query, callback)
+        db.query(query, callback);
     };
     delete_cliente(id_cliente: number, callback:queryCallback){
         const query = `DELETE from tb_clientes WHERE id_cliente = ${id_cliente}`
+        db.query(query,callback);
     }
 }
