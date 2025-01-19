cad076ea0d525c836f520f476b80c0c90c22b1ef
Gustavo Andrey <gustavoandrey@escolar.ifrn.edu.br>
2025-01-19 19:32:31 -0300

    Refactor getCarrinhoCliente method to ensure proper handling of API response

--- src/frontend/services/carrinhoService.ts
+++ src/frontend/services/carrinhoService.ts
@@ -4,20 +4,22 @@ import { ApiService } from "./apiService.js";
 export class CarrinhoService {
 	constructor(private _apiService: ApiService) {}
 
-	async getCarrinhoCliente(id_cliente: number) {
-		let ProdutosCarrinhoGET = await this._apiService.get(
-			`ProdutosCarrinho/${id_cliente}`)
+	async getCarrinhoCliente(id_cliente: number): Promise<ProdutoCarrinho[]> {
+		let ProdutosCarrinhoGET: any[] = await this._apiService.get(
+			`ProdutosCarrinho/${id_cliente}`
+		);
 		let ProdutosCarrinhoList: ProdutoCarrinho[] = [];
-		ProdutosCarrinhoGET.forEach((elemento: any) => {
-			let Produto: ProdutoCarrinho = new ProdutoCarrinho(
-				elemento.id_carrinho_produto,
-				elemento.id_cliente,
-				elemento.id_Product,
-				elemento.Qt_Product_Carrinho
-			);
-			ProdutosCarrinhoList.push(Produto);
-		});
-
+		if (Array.isArray(ProdutosCarrinhoGET)) {
+			ProdutosCarrinhoGET.forEach((elemento: any) => {
+				let Produto: ProdutoCarrinho = new ProdutoCarrinho(
+					elemento.id_carrinho_produto,
+					elemento.id_cliente,
+					elemento.id_Product,
+					elemento.Qt_Product_Carrinho
+				);
+				ProdutosCarrinhoList.push(Produto);
+			});
+		}
 		return ProdutosCarrinhoList;
 	}
 
