8dc8fe20fc413fc17048c6c247ba9b0d052f06e2
TrojanN63 <alves.tavares@escolar.ifrn.edu.br>
2025-01-19 10:36:19 -0300

    loginzão

--- src/backend/controllers/clientes_controle.ts
+++ src/backend/controllers/clientes_controle.ts
@@ -16,18 +16,17 @@ class ClienteControle extends ClienteModel{
     async logar_cliente(req:Request, res:Response){
         const email: string = req.body.email;
         const password: string = req.body.password;
-        db.query('SELECT * FROM tb_clientes WHERE cliente_email = ?', [email], async (err, results) => {
-            if (results.length === 0) {
-              return res.status(404).json({ message: 'Usuário não encontrado.' });
-            }
-            else{
-              if (password === results.cliente_log_senha){
-                this.obter_cliente(results.liente_email,res)
-              }else{
-                return res.status(401).json({ message: 'Credenciais inválidas.' });
-              }
-            }
-          })
+        this.get_Cliente(email, (err: MysqlError | null, Resultado: any) =>{
+          if (err) return res.send(err);
+          if (Resultado.length!=0 && password === Resultado[0].cliente_log_senha){
+            res.json(Resultado)
+          }else if (password != Resultado[0].cliente_log_senha){
+            res.send('Senha incorreta')
+          }
+          else {
+            res.send('Email não encontrado')
+          }
+        })
     }
     async criar_cliente(req: Request, res: Response){
         const cliente_email: string = req.body.cliente_email;
